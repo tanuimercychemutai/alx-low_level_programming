2165fe9a2b13ad1dd4892f6f86b1c75208b1cbe5
Tanui Chemutai <51640946+tanuimercychemutai@users.noreply.github.com>
2022-03-14 00:44:12 +0300

    Prints out a phrase

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-printf("with proper grammar, but the outcome is a piece of art,");
+#include <stdio.h>
+
+/**
+ * prints the phrase 
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
