28bf12c451656e30aac1d644f28fdf0460eb0d3b
tanuimercychemutai <ctanui62@gmail.com>
2022-03-14 08:42:34 -0700

    Using puts to print a phrase

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+*main -prints out "Programming is like bulding a multilingual puzzle
+*Returns 0: if exited correctly, negative if otherwise
+*/
+int main(void)
+{
+    puts(""Programming is like bulding a multilingual puzzle");
+    return 0;
+}
