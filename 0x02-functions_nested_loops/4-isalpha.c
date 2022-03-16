5efc1543b597d7781daaeda2709f3e43abe306b2
Tanui Chemutai <51640946+tanuimercychemutai@users.noreply.github.com>
2022-03-17 01:54:27 +0300

    Checks for lowercase or uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+/**
+ * _isalpha - checks if a letter is in lower case/upper case
+ * @c : character to check
+ * Return:0 or 1
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+
+}
