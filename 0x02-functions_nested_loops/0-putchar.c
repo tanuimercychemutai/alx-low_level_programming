c8b8d18b4b07afb94a6f19b02fa47927518cda8f
tanuimercychemutai <ctanui62@gmail.com>
2022-03-16 15:06:08 -0700

    Creating file 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - This is where the program begins
+* Description: It prints out _putchar, followed by a new line.
+* Return: 0 for successful program, otherwise, non zero.
+*/
+int main(void)
+{
+char word[8} = "_putchar";
+int i;
+
+for (i =0; i < 8; i++)
+{
+_putchar(word[i]);
+}
+_putchar(n);
+
+return (0);
+}
